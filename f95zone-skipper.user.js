879c04b106fdee4dbbed1d0a186d9b70ee1be5b0
Catie <130665030+Cat-Ling@users.noreply.github.com>
2024-01-26 12:24:21 +0530

    Update f95zone-skipper.user.js

--- f95zone-skipper.user.js
+++ f95zone-skipper.user.js
@@ -1,13 +1,16 @@
 // ==UserScript==
 // @name         F95-Zone skipper
 // @namespace    -
-// @version      1.2.1
+// @version      1.2.2
 // @description  This userscript skips masked f95 links for you.
 // @author       Cat-Ling
 // @homepageURL  https://github.com/Cat-Ling
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=f95zone.to
 // @match        https://f95zone.to/masked/*
+// @exclude https://f95zone.to/masked/
 // @grant        none
+// @license GPL-2.0
+// @supportURL https://github.com/Cat-Ling/f95zone-skipper/issues
 // @downloadURL https://raw.githubusercontent.com/Cat-Ling/f95zone-skipper/main/f95zone-skipper.user.js
 // @updateURL https://raw.githubusercontent.com/Cat-Ling/f95zone-skipper/main/f95zone-skipper.user.js
 // ==/UserScript==
