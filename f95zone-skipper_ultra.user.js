b6ecbca85e94a314fa5a4881b1e0d0f6c4d73d57
Catie <130665030+Cat-Ling@users.noreply.github.com>
2024-01-27 23:06:50 +0530

    remove jQuery dependence

--- f95zone-skipper_ultra.user.js
+++ f95zone-skipper_ultra.user.js
@@ -8,82 +8,81 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=f95zone.to
 // @match        https://f95zone.to/masked/*
 // @exclude      https://f95zone.to/masked/
-// @grant        none
 // @license      GPL-2.0
 // @supportURL   https://github.com/Cat-Ling/f95zone-skipper/issues
-// @downloadURL  -
-// @updateURL    -
+// @downloadURL https://raw.githubusercontent.com/Cat-Ling/f95zone-skipper/main/f95zone-skipper_ultra.user.js
+// @updateURL https://raw.githubusercontent.com/Cat-Ling/f95zone-skipper/main/f95zone-skipper_ultra.user.js
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    var $leaving = $(".leaving");
-    var $loading = $("#loading");
-    var $captcha = $("#captcha");
-    var $error = $("#error");
+    var $leaving = document.querySelector(".leaving");
+    var $loading = document.getElementById("loading");
+    var $captcha = document.getElementById("captcha");
+    var $error = document.getElementById("error");
 
     function handleError(title, message, retry) {
-        $error.html("<h2>" + title + "</h2><p>" + message + "</p>" + (retry ? '<p><a href="javascript:window.location.reload(true);">Retry</a></p>' : ""));
-        $loading.hide();
-        $error.show();
+        $error.innerHTML = "<h2>" + title + "</h2><p>" + message + "</p>" + (retry ? '<p><a href="javascript:window.location.reload(true);">Retry</a></p>' : "");
+        $loading.style.display = "none";
+        $error.style.display = "block";
     }
 
-    $leaving.css("width", $leaving.outerWidth());
-    $(".leaving-text").hide();
-    $loading.show();
+    $leaving.style.width = $leaving.offsetWidth + "px";
+    document.querySelector(".leaving-text").style.display = "none";
+    $loading.style.display = "block";
 
-    $.ajax({
-        method: "POST",
-        url: document.location.pathname,
-        data: { xhr: 1, download: 1 },
-        cache: false,
-        dataType: "json",
-        timeout: 20000,
-        success: function(response) {
-            switch (response.status) {
-                case "ok":
-                    window.location.href = response.msg;
-                    break;
-                case "error":
-                    handleError("Error", response.msg, true);
-                    break;
-                case "captcha":
-                    $captcha.show();
-                    handleCaptcha(response);
-                    break;
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", document.location.pathname, true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                var response = JSON.parse(xhr.responseText);
+                switch (response.status) {
+                    case "ok":
+                        window.location.href = response.msg;
+                        break;
+                    case "error":
+                        handleError("Error", response.msg, true);
+                        break;
+                    case "captcha":
+                        $captcha.style.display = "block";
+                        handleCaptcha(response);
+                        break;
+                }
+            } else {
+                handleError("Server Error", "Please try again in a few moments", true);
             }
-        },
-        error: function() {
-            handleError("Server Error", "Please try again in a few moments", true);
         }
-    });
+    };
+    xhr.send("xhr=1&download=1");
 
     function handleCaptcha(response) {
         grecaptcha.render("captcha", {
             theme: "dark",
             sitekey: "6LcwQ5kUAAAAAAI-_CXQtlnhdMjmFDt-MruZ2gov",
             callback: function(captchaResponse) {
-                $captcha.hide();
-                $loading.show();
-                $.ajax({
-                    method: "POST",
-                    url: document.location.pathname,
-                    data: { xhr: 1, download: 1, captcha: captchaResponse },
-                    cache: false,
-                    dataType: "json",
-                    timeout: 20000,
-                    success: function(response) {
-                        if (response.status !== "ok") {
-                            handleError("Captcha Error", response.msg, true);
+                $captcha.style.display = "none";
+                $loading.style.display = "block";
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", document.location.pathname, true);
+                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState == 4) {
+                        if (xhr.status == 200) {
+                            var response = JSON.parse(xhr.responseText);
+                            if (response.status !== "ok") {
+                                handleError("Captcha Error", response.msg, true);
+                            } else {
+                                window.location.href = response.msg;
+                            }
                         } else {
-                            window.location.href = response.msg;
+                            handleError("Server Error", "Please try again in a few moments", true);
                         }
-                    },
-                    error: function() {
-                        handleError("Server Error", "Please try again in a few moments", true);
                     }
-                });
+                };
+                xhr.send("xhr=1&download=1&captcha=" + captchaResponse);
             }
         });
     }
